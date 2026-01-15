68792b7d2a2d0f287de7ba99f871535822215604
bxStefan <stefanbvts@gmail.com>
2026-01-15 18:45:56 +0100

    added scalar api docs support

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +14,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options.Title = "pressAPI";
+        options.WithClassicLayout();
+        options.WithTheme(ScalarTheme.Default);
+        options.DefaultOpenAllTags = true;
+    });
 }
 
 app.UseHttpsRedirection();
